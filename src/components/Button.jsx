c654b5cc00c4c0d3b83a0c174e0e99190a620d14
Karla Agraz <karla.agraz@gmail.com>
2020-04-25 11:05:14 -0500

    state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const Button = props => {
-    const { text } = props;
-    return (
-        <div>
-            <button type="button">{text}</button>
-            <button type="button">{props.text}</button>
+class Button extends React.Component {
+    state = {
+        count: 0,
+    }
+    render () {
+        const { count } = this.state;
+        return (
+            <div>
+            <h1>Manzanas: {count} </h1>
+            <button type="button">Click</button>
         </div>
-    )
-};
+        )
+    }
+}
+
 
 export default Button;
